c79158320cab0ae1ffeab418c59c038eff04e44c
Alecpu92 <alessandro_belfiore@hotmail.it>
2023-05-16 17:39:06 +0200

    esercizio completo

--- main.js
+++ main.js
@@ -1,57 +1,121 @@
-const posts = [
+const postsContainer = document.getElementById("container");
+
+
+let posts = [
     {
-        "id": 1,
-        "content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
-        "media": "https://unsplash.it/600/300?image=171",
-        "author": {
-            "name": "Phil Mangione",
-            "image": "https://unsplash.it/300/300?image=15"
-        },
-        "likes": 80,
-        "created": "2021-06-25"
+        "userName" : "Phil Mangione",
+        "userPicture" : "https://unsplash.it/300/300?image=15",
+        "postDate" : "05/11/21",
+        "postText" : "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
+        "postImage" : "https://unsplash.it/600/300?image=171",
+        "likes" : 80
     },
+
     {
-        "id": 2,
-        "content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
-        "media": "https://unsplash.it/600/400?image=112",
-        "author": {
-            "name": "Sofia Perlari",
-            "image": "https://unsplash.it/300/300?image=10"
-        },
-        "likes": 120,
-        "created": "2021-09-03"
+        "userName" : "Valy Mex",
+        "userPicture" : "https://unsplash.it/300/300?image=50",
+        "postDate" : "04/05/21",
+        "postText" : "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
+        "postImage" : "https://unsplash.it/600/300?image=77",
+        "likes" : 10
     },
+
     {
-        "id": 3,
-        "content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
-        "media": "https://unsplash.it/600/400?image=234",
-        "author": {
-            "name": "Chiara Passaro",
-            "image": "https://unsplash.it/300/300?image=20"
-        },
-        "likes": 78,
-        "created": "2021-05-15"
+        "userName" : "FluIdo",
+        "userPicture" : "https://unsplash.it/300/300?image=27",
+        "postDate" : "31/01/21",
+        "postText" : "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
+        "postImage" : "''",
+        "likes" : 27
     },
+
     {
-        "id": 4,
-        "content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
-        "media": "https://unsplash.it/600/400?image=24",
-        "author": {
-            "name": "Luca Formicola",
-            "image": null
-        },
-        "likes": 56,
-        "created": "2021-04-03"
-    },
-    {
-        "id": 5,
-        "content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
-        "media": "https://unsplash.it/600/400?image=534",
-        "author": {
-            "name": "Alessandro Sainato",
-            "image": "https://unsplash.it/300/300?image=29"
-        },
-        "likes": 95,
-        "created": "2021-03-05"
+        "userName" : "Metallo",
+        "userPicture" : "https://unsplash.it/300/300?image=47",
+        "postDate" : "31/01/21",
+        "postText" : "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
+        "postImage" : "https://unsplash.it/300/300?image=52",
+        "likes" : 97
     }
-];
+]
+
+printPosts ();
+
+
+let likeBtns = document.querySelectorAll(".js-like-button");
+let likeCounter = document.querySelectorAll(".js-likes-counter");
+console.log(likeBtns);
+
+for(let i = 0; i < likeBtns.length; i++){
+
+    likeBtns[i].addEventListener("click",
+
+    function(){
+        let indexLikes = posts[i].likes;
+        if(this.className.includes("like-button--liked")){ 
+
+            likeBtns[i].classList.remove("like-button--liked");
+
+            likeCounter[i].innerHTML = indexLikes;
+
+        }else{
+            indexLikes += 1;
+
+            likeCounter[i].innerHTML = indexLikes;
+    
+            likeBtns[i].classList.add("like-button--liked");
+        }
+
+    }
+    );
+}
+
+
+
+function printPosts (){
+    for (let i = 0; i < posts.length; i++){
+        let indexPosts = posts[i];
+
+        let {userName, userPicture, postDate, postText, likes, postImage} = indexPosts;
+        
+        printInHtml(userName, userPicture, postDate, postText, likes, i, postImage);
+        console.log(postImage);
+    }
+}
+
+
+function printInHtml(userName, userPicture, postDate, postText, likes, indexLikes, postImage){
+    postsContainer.innerHTML += `
+
+        <div class="post">
+            <div class="post__header">
+                <div class="post-meta">                    
+                    <div class="post-meta__icon">
+                        <img class="profile-pic" src=${userPicture} alt=${userName}>                    
+                    </div>
+                    <div class="post-meta__data">
+                        <div class="post-meta__author">${userName}</div>
+                        <div class="post-meta__time">${postDate}</div>
+                    </div>                    
+                </div>
+            </div>
+            <div class="post__text">${postText}</div>
+            <div class="post__image">
+                <img src=${postImage} alt=""></img>
+            </div>
+            <div class="post__footer">
+                <div class="likes js-likes">
+                    <div class="likes__cta">
+                        <a class="like-button  js-like-button" data-postid="${indexLikes}">
+                            <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                            <span class="like-button__label">Mi Piace</span>
+                        </a>
+                    </div>
+                    <div class="likes__counter">
+                        Piace a <b id="like-counter-${indexLikes}" class="js-likes-counter">${likes}</b> persone
+                    </div>
+                </div> 
+            </div>            
+        </div>
+    `
+}
